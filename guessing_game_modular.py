6b2e384ec8a4c504fe42925ab29ebb66cdded506
TanjaCG <tanja.cukgaber@gmail.com>
2020-06-09 10:36:04 +0200

    Update guessing_game_modular.py

--- guessing_game_modular.py
+++ guessing_game_modular.py
@@ -1,15 +1,20 @@
-from guessing_game_functions import player_name, game_play, get_scores
+from guessing_game_functions import player_name, game_play, get_scores, get_top_scores
 
-player_name
 print("Hello, {}!" .format(player_name))
 
-
 while True:
-    question = input("What would you like to do? \n A) Play a game. \n B) See best scores. \n C) Nothing. \n ")
+    question = input("What would you want to do? \n A) Play a game. \n "
+                     "B) See best scores. \n C) See all scores. \n D) Nothing. ")
 
     if question.upper() == "A":
         game_play()
     elif question.upper() == "B":
-        get_scores()
+        get_top_scores()
+    elif question.upper() == "C":
+        for all_scores in get_scores():
+            print(str(all_scores["attempts"]) + " attempts, date: " + all_scores.get("datetime") +
+                  ", player name: " + all_scores.get("player name") +
+                  ", wrong guesses: " + str(all_scores.get("wrong_guess")))
     else:
+        print("Bye!")
         break
